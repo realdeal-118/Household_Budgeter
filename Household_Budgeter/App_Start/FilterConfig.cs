5945c9fb8b5c625018d4406c786041295a73ce0c
realdeal-118 <realdeal118@hotmail.com>
2016-07-14 16:58:13 -0400

    First1

--- Household_Budgeter/App_Start/FilterConfig.cs
+++ Household_Budgeter/App_Start/FilterConfig.cs
@@ -3,6 +3,7 @@ using System.Web.Mvc;
 
 namespace Household_Budgeter
 {
+    //This is the Filter Config file//
     public class FilterConfig
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
